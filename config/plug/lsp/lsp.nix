7b278165bdf6a7028de1d736d84eadc0cd80fc18
Eduardo Loback <eduardolloback@gmail.com>
2024-07-06 20:25:30 -0300

    fix: lsp servers

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -9,10 +9,7 @@
         lua-ls = { enable = true; };
         nil-ls = { enable = true; };
         marksman = { enable = true; };
-        pyright = { enable = true; };
-        gopls = { enable = true; };
-        terraformls = { enable = true; };
-        tsserver = { enable = false; };
+        tsserver = { enable = true; };
         yamlls = {
           enable = true;
         };
