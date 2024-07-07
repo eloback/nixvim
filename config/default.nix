5c95b638bc97b8735fa73f95a126ddb911778c62
Eduardo Loback <eduardolloback@gmail.com>
2024-07-06 22:50:52 -0300

    fix: none-ls -> conform, conform nix bug solved

--- config/default.nix
+++ config/default.nix
@@ -22,12 +22,12 @@
     ./plug/git/lazygit.nix
     ./plug/git/worktree.nix
 
-    # ./plug/lsp/conform.nix
+    ./plug/lsp/conform.nix
     ./plug/lsp/fidget.nix
     ./plug/lsp/hlchunk.nix
     ./plug/lsp/lsp.nix
     ./plug/lsp/lspsaga.nix
-    ./plug/lsp/none-ls.nix
+    # ./plug/lsp/none-ls.nix
     ./plug/lsp/trouble.nix
 
     ./plug/snippets/luasnip.nix
@@ -79,7 +79,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "catppuccin";
+    theme = "paradise";
     extraConfigLua = '' 
       _G.theme = "${config.theme}"
     '';
