672d571083de0fefb54a9a936d0a6ae70d29a0cf
Eduardo Loback <eduardolloback@gmail.com>
2024-07-17 17:52:32 -0300

    fix: cmp keybinds

--- config/plug/completion/cmp.nix
+++ config/plug/completion/cmp.nix
@@ -49,8 +49,8 @@
                   ['<C-Space>'] = cmp.mapping.complete(),
                   ['<C-CR>'] = cmp.mapping.abort(),
                   ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                  [ 'Down' ] = cmp.mapping.select_next_item(),
-                  [ 'Up' ] = cmp.mapping.select_prev_item(),
+                  [ '<Down>' ] = cmp.mapping.select_next_item(),
+                  [ '<Up>' ] = cmp.mapping.select_prev_item(),
               })
             '';
           };
