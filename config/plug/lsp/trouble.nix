f171e8545e0146a47010f15f026424fc351e2d75
Eduardo Loback <eduardolloback@gmail.com>
2024-07-18 15:38:46 -0300

    Revert "feat: basic trouble keys"
    This reverts commit e12e231fcd1108445006cac334528fe1028a7e69.

--- config/plug/lsp/trouble.nix
+++ config/plug/lsp/trouble.nix
@@ -1,27 +1,5 @@
 {
   plugins.trouble = {
     enable = true;
-    settings = {
-      auto_close = true;
-      auto_preview = true;
-    };
   };
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>xx";
-      action = "<cmd>Trouble document_diagnostics<cr>";
-      options = {
-        desc = "Diagnostics  (Buffer)";
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>xX";
-      action = "<cmd>Trouble workspace_diagnostics<cr>";
-      options = {
-        desc = "Diagnostics";
-      };
-    }
-  ];
 }
