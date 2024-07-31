016d2cdb55870557e7f73dd8f02b21c041cca839
Eduardo Loback <eduardolloback@gmail.com>
2024-07-31 12:37:13 -0300

    (markview): bumb

--- config/plug/utils/markview.nix
+++ config/plug/utils/markview.nix
@@ -6,8 +6,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "OXY2DEV";
         repo = "markview.nvim";
-        rev = "8537f7f03e4683b9ef98202e7a0286373a5b7135";
-        hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
+        rev = "d0ccc97b5c988fb28e5033abdf7b832b9dfaf897";
+        hash = "sha256-mhRg/cszW/3oXdC1yvbpCeqWQA9WLW5FvcqGd/wBTnE=";
       };
     })
   ];
