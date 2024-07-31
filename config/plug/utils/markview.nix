534f373fbbf656d5810367d19a0784ed2d869e96
Eduardo Loback <eduardolloback@gmail.com>
2024-07-30 22:03:03 -0300

    chore: bumb

--- config/plug/utils/markview.nix
+++ config/plug/utils/markview.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }: {
-  extraPlugins = with pkgs.vimUtils; [
-    (buildVimPlugin {
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
       pname = "markview.nvim";
       version = "0.0.1";
       src = pkgs.fetchFromGitHub {
         owner = "OXY2DEV";
         repo = "markview.nvim";
-        rev = "f60219dce7283192d549f21847fcf8537bf6d260";
-        hash = "sha256-E1lHSjbnOOIeieaFJ+INvxJHCbfVS3mwbQ6wrlKeGSQ=";
+        rev = "8537f7f03e4683b9ef98202e7a0286373a5b7135";
+        hash = "sha256-w6yn8aNcJMLRbzaRuj3gj4x2J/20wUROLM6j39wpZek=";
       };
     })
   ];
