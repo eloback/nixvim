250a256ae3209007fa99366acd85536429bca52a
Eduardo Loback <eduardolloback@gmail.com>
2024-08-09 10:48:55 -0300

    chore: enable lsp normal keybinds again (disable lspsaga)

--- config/plug/lsp/lsp.nix
+++ config/plug/lsp/lsp.nix
@@ -35,56 +35,90 @@
       keymaps = {
         silent = true;
         lspBuf = {
-          # gd = {
-          #   action = "definition";
-          #   desc = "Goto Definition";
-          # };
-          # gr = {
-          #   action = "references";
-          #   desc = "Goto References";
-          # };
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
           gD = {
             action = "declaration";
             desc = "Goto Declaration";
           };
-          # gI = {
-          #   action = "implementation";
-          #   desc = "Goto Implementation";
-          # };
-          # gT = {
-          #   action = "type_definition";
-          #   desc = "Type Definition";
-          # };
-          # K = {
-          #   action = "hover";
-          #   desc = "Hover";
-          # };
-          # "<leader>cw" = {
-          #   action = "workspace_symbol";
-          #   desc = "Workspace Symbol";
-          # };
-          # "<leader>cr" = {
-          #   action = "rename";
-          #   desc = "Rename";
-          # };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
         };
         diagnostic = {
-          # "<leader>cd" = {
-          #   action = "open_float";
-          #   desc = "Line Diagnostics";
-          # };
-          # "[d" = {
-          #   action = "goto_next";
-          #   desc = "Next Diagnostic";
-          # };
-          # "]d" = {
-          #   action = "goto_prev";
-          #   desc = "Previous Diagnostic";
-          # };
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
         };
       };
     };
   };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action = "<cmd>Telescope lsp_definitions<cr>";
+      options = {
+        desc = "Goto Definition";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gr";
+      action = "<cmd>Telescope lsp_references<cr>";
+      options = {
+        desc = "Goto References";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gI";
+      action = "<cmd>Telescope lsp_implementations<cr>";
+      options = {
+        desc = "Goto Implementations";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gT";
+      action = "<cmd>Telescope lsp_type_definitions<cr>";
+      options = {
+        desc = "Goto Type Definitions";
+      };
+    }
+  ];
   extraConfigLua = ''
     local _border = "rounded"
 
